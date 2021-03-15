21275e4ff9b5c18aa021c9aa48fb952517a37cc6
Nazarshia2889 <arshia.nayebnazar@yahoo.com>
2021-03-14 21:58:05 -0700

    addition

--- src/_01_StringBuilder/_03_StringBuilder.java
+++ src/_01_StringBuilder/_03_StringBuilder.java
@@ -4,7 +4,8 @@ package _01_StringBuilder;
 public class _03_StringBuilder {
     
     public static String append(String str, char[] characters) {
-        return null;
+    	StringBuilder builder = new StringBuilder();
+        return "";
     }
     
     public static String reverse(String str) {
